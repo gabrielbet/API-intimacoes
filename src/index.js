68853ed0a05b8d65230f07ba0f49514a53af3a26
gabrielbet <55458497+gabrielbet@users.noreply.github.com>
2020-06-21 17:17:12 -0300

    process.env.PORT

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
